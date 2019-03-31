78cfd537cfab0a3e33b887d777c8d679fa00b8a5
Rahul Gupta <rguptemail10@gmail.com>
2019-03-31 16:30:06 -0400

    Started with the uploader page

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 @app.route('/upload')
 def upload_image():
     return render_template('upload.html')
+def show():
+    return render_template('upload.html')
+
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
@@ -15,9 +18,10 @@ def upload_file():
         f = request.files['file']
         f.save(f.filename)
 
-    return process_image(f.filename)
     return render_template('test.html')
+    return process_image(f.filename)
+
 
 if __name__ == '__main__':
     #set debug to false if using in production environment
-    app.run(debug = False, host='0.0.0.0')
+    app.run(debug = True, host='0.0.0.0')
